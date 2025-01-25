3de71d27d5444c58e834b9de535795aa0899ed50
Junaid Ul Hassan <116207188+junaidulhassan@users.noreply.github.com>
2025-01-25 15:16:07 +0500

    Update LLM.py

--- LLM.py
+++ LLM.py
@@ -1,7 +1,6 @@
 import warnings as wn
 # Ignore warning messages
 wn.filterwarnings('ignore')
-
 # Langchain is the library for support LLMs doing advance tasks
 from langchain_community.llms import HuggingFaceHub
 from api_token import LargeLanguageModel
